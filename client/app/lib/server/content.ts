e07481b984e665e30e345a32984ff49fad3c857a
Mark Florence <mflo999@gmail.com>
2017-05-07 11:11:25 -0400

    Cleaned up content to be able to deploy SPA dist from "anywhere"

--- client/app/lib/server/content.ts
+++ client/app/lib/server/content.ts
@@ -1,14 +1,36 @@
 import * as express from 'express';
+import * as fs from 'fs';
 import * as path from 'path';
 
 /**
  * Deploy static Angular-generated content
  */
 
+ const RETRIES = 5;
+
 export function content(req: express.Request,
                         res: express.Response) {
+
     const name = req.path.substring(1);
-    if (name)
-      res.sendFile(path.join(__dirname, '../../../../dist', name));
-    else res.sendFile(path.join(__dirname, '../../../../dist', 'home.html'));
+
+    // this is awkward: we know dist is SOMEWHERE based on build config
+    // we accept the over head because we are dealing with an SPA
+    // and the content gets loaded only once
+
+    let file, i, pfx = '';
+    for (i = 0; i < RETRIES; i++) {
+      file = path.join(__dirname, `${pfx}dist`, name? name : 'home.html');
+      try {
+        fs.accessSync(file);
+        break;
+      }
+      catch (ignored) { }
+      pfx = `../${pfx}`;
+    }
+
+    // only try so many times then give up
+
+    if (i === RETRIES)
+      res.sendStatus(404);
+    else res.sendFile(file);
 };
