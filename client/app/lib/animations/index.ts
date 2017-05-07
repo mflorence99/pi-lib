cf970fa8c56134f4e428336a2056826891ccaf4c
Mark Florence <mflo999@gmail.com>
2017-05-07 04:51:44 -0400

    Added some comments

--- client/app/lib/animations/index.ts
+++ client/app/lib/animations/index.ts
@@ -2,19 +2,22 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 
 /**
  * Common animation functions
+ *
+ * NOTE: why any return?
+ * http://stackoverflow.com/questions/
+ *   43335336/error-ts4058-return-type-of-exported-function-has-or-is-using-name-x-from-exter
+ *
+ * NOTE: this is all wrong but fix it when Angular 4.1 is released with route animations
  */
 
-// why any return?
-// http://stackoverflow.com/questions/
-//  43335336/error-ts4058-return-type-of-exported-function-has-or-is-using-name-x-from-exter
 export function flyInOut(): any {
   return trigger('flyInOut', [
     state('in', style({transform: 'translate(0, 0)'})),
     transition('void => *', [
-      animate(100, style({transform: 'translate(-100%, 0)'}))
+      animate(250, style({transform: 'translate(-100%, 0)'}))
     ]),
     transition('* => void', [
-      animate(100, style({transform: 'translate(100%, 0)'}))
+      animate(250, style({transform: 'translate(100%, 0)'}))
     ])
   ]);
 }
