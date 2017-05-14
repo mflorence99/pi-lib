1a0a897d72750fae7b77090e7171c7f1056db79c
Mark Florence <mflo999@gmail.com>
2017-05-14 17:15:33 -0400

    Added show/hide animation

--- client/app/lib/animations/index.ts
+++ client/app/lib/animations/index.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import { AnimationEntryMetadata } from '@angular/core';
 
@@ -12,5 +12,14 @@ export function routeAnimation(): AnimationEntryMetadata {
       style({transform: 'translateX(-100%)'}),
       animate('0.25s ease-in-out', style({transform: 'translateX(0%)'}))
     ])
+  ]);
+}
+
+
+export function showHideAnimation(): AnimationEntryMetadata {
+  return trigger('showHide', [
+    state('shown', style({height: '*', opacity: 1})),
+    state('hidden', style({height: 0, opacity: 0})),
+    transition('* => *', animate('0.25s ease-in-out'))
   ]);
 }
