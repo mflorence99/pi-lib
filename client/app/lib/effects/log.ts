98a5ac186c06d62af286d046f189789baf989c08
Mark Florence <mflo999@gmail.com>
2017-05-15 05:14:50 -0400

    Correct ngc error

--- client/app/lib/effects/log.ts
+++ client/app/lib/effects/log.ts
@@ -2,7 +2,9 @@ import 'rxjs/add/operator/do';
 
 import { Actions, Effect } from '@ngrx/effects';
 
+import { Action } from '@ngrx/store';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
 
 /**
@@ -16,7 +18,7 @@ export class LogEffects {
    * Simply log all actions in dev mode
    */
 
-  @Effect({dispatch: false}) logActions = this.actions
+  @Effect({dispatch: false}) logActions: Observable<Action> = this.actions
     .filter(action => !environment.production)
     .do((action => console.log(`%c ${action.type}`, 'color: green', action.payload)));
 
