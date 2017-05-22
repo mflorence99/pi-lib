e1190e0c00c81c7943a2137dd6eebbdbfe233552
Mark Florence <mflo999@gmail.com>
2017-05-22 05:24:02 -0400

    Add these back

---
+++ client/app/lib/components/route-animation.ts
@@ -0,0 +1,27 @@
+import * as router from '@ngrx/router-store';
+
+import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+
+import { routeAnimation } from '../animations';
+
+/**
+ * This is all crap until Angular 4.1 releases route transitions
+ */
+
+@Component({
+  animations: [routeAnimation()],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'lib-route-animation',
+  styleUrls: ['route-animation.less'],
+  templateUrl: 'route-animation.html'
+})
+
+export class RouteAnimationComponent {
+
+  @HostBinding('@routeAnimation') get trigger() {
+    return this.routerState.path;
+  }
+
+  @Input() routerState: router.RouterState = router.initialState;
+
+}
