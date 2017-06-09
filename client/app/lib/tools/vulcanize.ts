e67e16b84e90bf872ae175b10c4a153ade3bbf1d
Mark Florence <mflo999@gmail.com>
2017-06-09 10:31:16 -0400

    Simplify package.json

--- client/app/lib/tools/vulcanize.ts
+++ client/app/lib/tools/vulcanize.ts
@@ -1,12 +1,12 @@
 import * as clean from 'gulp-clean-css';
 import * as gulp from 'gulp';
 import * as gulpif from 'gulp-if';
-import * as harmony from 'uglify-js-harmony';
-import * as minify from 'gulp-htmlmin';
+import * as minifyHTML from 'gulp-htmlmin';
+import * as minifyJS from 'gulp-uglify/minifier';
 import * as path from 'path';
 import * as polymer from 'polymer-build';
 import * as rename from 'gulp-rename';
-import * as uglify from 'gulp-uglify/minifier';
+import * as uglify from 'uglify-js-harmony';
 import * as util from 'gulp-util';
 import * as vulcanize from 'gulp-vulcanize';
 import * as yargs from 'yargs';
@@ -28,18 +28,18 @@ gulp.task('default', () => {
     .pipe(splitter.split())
     // NOTE: js minification effectively a no-op
     // Uncaught TypeError: Cannot read property 'bindings' of undefined
-    .pipe(gulpif(/\.js$/, uglify({
+    .pipe(gulpif(/\.js$/, minifyJS({
         mangle: false
-      }, harmony)))
+      }, uglify)))
     .on('error', err => util.log(util.colors.red('[JS Error]'), err.toString()))
     .pipe(gulpif(/\.css$/, clean()))
     .on('error', err => util.log(util.colors.red('[CSS Error]'), err.toString()))
-    .pipe(gulpif(/\.html$/, minify({
+    .pipe(gulpif(/\.html$/, minifyHTML({
         caseSensitive: true,
         collapseInlineTagWhitespace: true,
         collapseWhitespace: true,
         decodeEntities: true,
-        minifyCSS: true,
+        minifyHTMLCSS: true,
         removeAttributeQuotes: true,
         removeComments: true
       })))
